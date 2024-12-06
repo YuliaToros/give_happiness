44852e1df1d361a694a6ec8bcffe8e7ebf651554
YuliaToros <yulia.elb@gmail.com>
2024-12-06 15:44:17 +0300

    button home page

--- client/src/pages/HomePage/ui/HomePage.tsx
+++ client/src/pages/HomePage/ui/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { CLIENT_ROUTES } from "@/app/router";
 import { CarouselBanner } from '@/widgets';
+import { Link } from 'react-router-dom';
 
 // Импортируем изображения
 import banner1 from '../assets/banner_null_1.png';
 import banner2 from '../assets/banner_null_2.png';
 import banner3 from '../assets/banner_null_3.png';
+
 
 // Интерфейс для изображений
 interface Image {
@@ -29,11 +32,6 @@ export const HomePage: React.FC = () => {
   ];
 
   return (
-    // <>
-    //   <CarouselBanner images={images} />
-    //   <p>Подари счастье – маркет-плейс подарочных сертификатов в твоем городе!</p>
-    //   <button> Посмотреть каталог</button>
-    // </>
     <div className="container-fluid">
     <div className="row">
       <div className="col-12">
@@ -47,7 +45,7 @@ export const HomePage: React.FC = () => {
     </div>
     <div className="row mt-3">
       <div className="col-12 text-center">
-        <button className="btn btn-primary btn-lg">Посмотреть сертификаты</button>
+        <Link to={CLIENT_ROUTES.SERTIFICATES} ><button className="btn btn-primary btn-lg">Посмотреть сертификаты</button></Link>
       </div>
     </div>
   </div>
